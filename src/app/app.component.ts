056cf19e636c85a95eaa73ce86ecf79595f0819c
C-DA <corentin-dargent@hotmail.com>
2020-06-25 23:10:07 +0200

    feat(): install boostrap and xlsx reader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+type AOA = any[][];
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'games-app';
+  data: AOA = [
+    [1, 2],
+    [3, 4]
+  ];
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
+
+  public ngOnInit(): void {
+    const inputFile = document.querySelector('input');
+    inputFile.addEventListener('change', (evt: any) => {
+      console.log(inputFile.files);
+      const target: DataTransfer = <DataTransfer>evt.target;
+      if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+      const reader: FileReader = new FileReader();
+      reader.onload = (e: any) => {
+        /* read workbook */
+        const bstr: string = e.target.result;
+        const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+        /* grab first sheet */
+        const wsname: string = wb.SheetNames[2];
+        const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+        /* save data */
+        this.data = <AOA>XLSX.utils.sheet_to_json(ws, { header: 1 });
+        console.log(this.data);
+      };
+      reader.readAsBinaryString(target.files[0]);
+    });
+  }
+
+  export(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
-import { en_US, NgZorroAntdModule, NZ_I18N } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
 import { services } from './services';
 
@@ -15,7 +14,7 @@ registerLocaleData(localFr);
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, NgZorroAntdModule, FormsModule, HttpClientModule, BrowserAnimationsModule, TranslateModule],
-  providers: [{ provide: NZ_I18N, useValue: en_US }, services]
+  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule, TranslateModule],
+  providers: [services]
 })
 export class AppModule {}
